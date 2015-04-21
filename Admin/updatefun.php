8e009639329667eb49bdaf9a255192908e383572
Ryan-lawgic <RyanPro@MacBook-Pro.local>
2015-04-21 19:00:55 +0800

    Update fun() works!
    It works, definitely!

--- Admin/updatefun.php
+++ Admin/updatefun.php
@@ -1,42 +1,11 @@
 <?php
-$con = mysql_connect("localhost","root","");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+    mysql_connect("localhost", "root","") or die(mysql_error()); //Connect to server
+    mysql_select_db("first_db") or die("Cannot connect to database"); //Connect to database
+    
+    session_start();
+    $name = $_POST['name'];
+    $id = $_SESSION['id'];
 
-// some code
-
-if($_POST){
-    try{
-  
-        //write query
-        //in this case, it seemed like we have so many fields to pass and
-        //its kinda better if we'll label them and not use question marks
-        //like what we used here
-        $query = "update product
-                    set name = :name
-                    where id = :id";
-  
-        //prepare query for excecution
-        $stmt = $con->prepare($query);
-  
-        //bind the parameters
-        $stmt->bindParam(':name', $_POST['name']);
-        //$stmt->bindParam(':lastname', $_POST['lastname']);
-        //$stmt->bindParam(':username', $_POST['username']);
-        //$stmt->bindParam(':password', $_POST['password']);
-        //$stmt->bindParam(':id', $_POST['id']);
-  
-        // Execute the query
-        if($stmt->execute()){
-            echo "Record was updated.";
-        }else{
-            die('Unable to update record.');
-        }
-  
-    }catch(PDOException $exception){ //to handle error
-        echo "Error: " . $exception->getMessage();
-    }
-}
+    mysql_query("UPDATE product SET name='$name' WHERE id='$id'") ;
 ?>
+
